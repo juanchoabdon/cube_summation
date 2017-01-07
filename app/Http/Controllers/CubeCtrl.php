1fde32db3bdcbe056b2e07c9c35c132581061f5a
Juan Diego <juan.sanchez@cerebapp.com>
2017-01-07 18:59:26 -0500

    Creada en controlador principal funcion que recibe request y devuelve un json!

--- app/Http/Controllers/CubeCtrl.php
+++ app/Http/Controllers/CubeCtrl.php
@@ -11,5 +11,27 @@ use App\Models\Cube;
 class CubeCtrl extends Controller
 {
 
-  
+   // Acción que ejecuta las operaciones según el input  y retorna un json con el output
+	public function  summation(Request $input){
+
+		$output = "";
+	foreach($input->input('inputs') as $inputs){
+			$cube = new Cube($inputs["n"]);
+
+
+			foreach($inputs["operations"] as $operation){
+
+				$params = explode(" ", $operation["params"]);
+
+				if($operation["operation_name"] == "query"){
+					$output = $cube->query($params[0], $params[1], $params[2], $params[3], $params[4], $params[5]) . ",";
+				}
+				elseif($operation["operation_name"] == "update"){
+					$cube->update($params[0], $params[1], $params[2], $params[3]);
+				}
+			}
+		}
+ 
+		return response()->json(["output" => $output]);
+	}
 }
