9e75f5bd3851f071f68577345d6d1370b98bcf42
Juan Diego <juan.sanchez@cerebapp.com>
2017-01-07 19:05:41 -0500

    Añadidas 4 pruebas

--- tests/CubeTest.php
+++ tests/CubeTest.php
@@ -4,6 +4,39 @@
 
 class CubeTest extends TestCase {
 
-	
+	function test1(){
+		$cube = new App\Models\Cube(4);
 
+		$cube->update(2, 2, 2, 4);
+		$this->assertEquals(4, $cube->query(1, 1, 1, 3, 3, 3));
+	}
+
+	function test2(){
+		$cube = new App\Models\Cube(4);
+
+		$cube->update(2, 2, 2, 4);
+		$cube->update(1, 1, 1, 23);
+
+		$this->assertEquals(4, $cube->query(2, 2, 2, 4, 4, 4));
+	}
+
+
+	function test3(){
+		$cube = new App\Models\Cube(4);
+
+		$cube->update(2, 2, 2, 4);
+		$cube->update(1, 1, 1, 23);
+
+		$this->assertEquals(27, $cube->query(1, 1, 1, 3, 3, 3));
+	}
+
+	function test4(){
+		$cube = new App\Models\Cube(4);
+
+		$cube->update(2, 2, 2, 1);
+
+		$this->assertEquals(0, $cube->query(1, 1, 1, 1, 1, 1));
+	}
+
+	
 }
