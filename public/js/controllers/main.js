9529939faca08e187b7311c6d9f736387d9aaa69
Juan Diego <juan.sanchez@cerebapp.com>
2017-01-07 19:26:55 -0500

    Interactividad con angularjs

--- public/js/controllers/main.js
+++ public/js/controllers/main.js
@@ -1,4 +1,24 @@
 app.controller("MainController", function($scope, $http, PROCESS_URL, $sce){
 
+	$scope.test_cases_num;
+
+	$scope.set_m_n = function(test_case){
+		test_case.n = test_case.m_n.split(" ")[0];
+		m = test_case.m_n.split(" ")[1];
+
+		test_case.operations = [];
+		for(var i = 0; i < m; i++){
+			test_case.operations.push({operation_name: "", params: ""});
+		}
+
+	}
+
+	$scope.create_test_cases = function(){
+		$scope.test_cases = [];
+		for(i = 0; i < $scope.test_cases_num; i++){
+			$scope.test_cases.push({m_n: null, n: null, operations: []})
+		}
+
+	}
 
 })
