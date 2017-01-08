a8b814fb14c60843aef9c7dd490649bd2b1c5ae5
Juan Diego <juan.sanchez@cerebapp.com>
2017-01-07 19:21:26 -0500

    Creada vista app angularjs

---
+++ resources/views/welcome.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html ng-app="cubeSummation">
+    <head>
+        <title>Cube Summation</title>
+       <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.8/css/materialize.min.css">
+
+
+    </head>
+    <body>
+
+
+
+    
+<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.8/js/materialize.min.js"></script>
+
+<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.6/angular.js"></script>
+
+<script src="js/app.js"></script>
+<script src="js/controllers/main.js"></script>
+<script type="text/javascript">
+
+
+$('select').material_select();
+
+
+
+</script>
+    </body>
+
+</html>
